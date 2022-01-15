674e5254905b1091c4c4e79ee67817f805b5167b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-16 00:01:33 +0500

    work complete

---
+++ tinder/src/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton } from '@mui/material';
+import ForumIcon from '@mui/icons-material/Forum';
+function Header() {
+    return (
+        <div className='header'>
+            <IconButton>
+            <AccountCircleIcon fontSize='large' className='header__icon'/>
+            </IconButton>
+            <img className='header__logo'
+             src='https://logodownload.org/wp-content/uploads/2020/09/tinder-logo.png'
+              alt=''
+             />
+             <IconButton>
+            <ForumIcon fontSize='large' className='header__icon'/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
