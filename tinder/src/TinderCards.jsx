1d421127e399562108b7c4449e8a2c5c6f456c30
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-18 00:27:15 +0500

    work complete

--- tinder/src/TinderCards.jsx
+++ tinder/src/TinderCards.jsx
@@ -4,12 +4,17 @@ import "./TinderCards.css";
 
 function TinderCards() {
   const [people, setPeople] = useState([
-    
+    {
+      name: "Virat Kohli",
+      image_url:
+        "https://gumlet.assettype.com/nationalherald%2F2021-09%2F92184028-a569-4063-b311-7bcb984824f4%2FVirat_Kohli_to_step_down_as_Indian_captain.jpg?rect=0%2C2%2C806%2C423&w=1200&auto=format%2Ccompress&ogImage=true",
+    }, 
     {
       name: "Babar Azam",
       image_url:
         "https://www.thenews.com.pk/assets/uploads/updates/2021-06-01/843144_134147_Babar-Azam-hero-loook_updates.jpg",
-    },{
+    },
+    {
       name: "Leo Messi",
       image_url:
         "https://www.fcbarcelonanoticias.com/uploads/s1/12/78/51/5/paris-saint-germain-v-lille-osc-ligue-1-uber-eats-1.jpeg?mrf-size=m",
