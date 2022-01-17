7ce99de7af8d2828db89b0edcd908ab58d1498c5
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-18 00:23:53 +0500

    work complete

--- tinder/src/TinderCards.jsx
+++ tinder/src/TinderCards.jsx
@@ -4,16 +4,21 @@ import "./TinderCards.css";
 
 function TinderCards() {
   const [people, setPeople] = useState([
+    
+    {
+      name: "Babar Azam",
+      image_url:
+        "https://www.thenews.com.pk/assets/uploads/updates/2021-06-01/843144_134147_Babar-Azam-hero-loook_updates.jpg",
+    },{
+      name: "Leo Messi",
+      image_url:
+        "https://www.fcbarcelonanoticias.com/uploads/s1/12/78/51/5/paris-saint-germain-v-lille-osc-ligue-1-uber-eats-1.jpeg?mrf-size=m",
+    },
     {
       name: "Cristiano Ronaldo",
       image_url:
         "https://static.standard.co.uk/2021/11/28/16/newFile-2.jpg?width=1200",
     },
-    {
-        name: "Leo Messi",
-        image_url:
-          "https://www.fcbarcelonanoticias.com/uploads/s1/12/78/51/5/paris-saint-germain-v-lille-osc-ligue-1-uber-eats-1.jpeg?mrf-size=m",
-      },
 
   ]);
   const swiped = (direction, nameToDelete) => {
