79010a1127415325c498dcf00e38e1854ddf7b53
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-22 00:45:06 +0500

    work complete

--- server.mjs
+++ server.mjs
@@ -1,3 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
-console.log('Muhammad Fahad Siddiqui 2019-SE-206')
+console.log('Muhammad Fahad Siddiqui ')
+
+// App Config
+const app = express()
+const PORT= process.env.port || 7000
+
+//Middleware
+
+//DB Config
+
+//Api Endpoints
+app.get('/',(res,req)=> res.status(200).send('Hello Fahad Bhai'))
+
+//Listener
+app.listen(PORT,()=>console.log(`YOUR APP IS BEiNG LISTENED AT PORT ${PORT}`) )
