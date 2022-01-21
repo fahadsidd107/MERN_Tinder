05f800286c3bfb4ca96ca3a3425fd0847f0f32d0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-22 01:15:37 +0500

    work complete

--- server.mjs
+++ server.mjs
@@ -5,11 +5,16 @@ console.log('Muhammad Fahad Siddiqui ')
 // App Config
 const app = express()
 const PORT= process.env.port || 7000
-
+const db_url = 'mongodb+srv://admin:admin@cluster0.zkqln.mongodb.net/tinderdb?retryWrites=true&w=majority'
 //Middleware
 
 //DB Config
-
+mongoose.connect(db_url
+    // ,{
+    // useNewUrlParser:true,
+    // useCreateIndex:true,
+    // useUnifiedTopology:true,}
+    )
 //Api Endpoints
 app.get('/',(res,req)=> res.status(200).send('Hello Fahad Bhai'))
 
