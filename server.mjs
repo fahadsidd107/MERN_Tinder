2414d655a584a91f1f07c87df58922cdeb5739fa
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-23 16:39:58 +0500

    work complete

--- server.mjs
+++ server.mjs
@@ -7,6 +7,7 @@ const app = express()
 const PORT= process.env.port || 7000
 const db_url = 'mongodb+srv://admin:admin@cluster0.zkqln.mongodb.net/tinderdb?retryWrites=true&w=majority'
 //Middleware
+app.use(express.json())
 
 //DB Config
 mongoose.connect(db_url
