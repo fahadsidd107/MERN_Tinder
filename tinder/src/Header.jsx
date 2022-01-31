24093db377f22b37c90819bec9623d2442ceb624
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-31 19:42:22 +0500

    work complete

--- tinder/src/Header.jsx
+++ tinder/src/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
             <IconButton>
             <AccountCircleIcon fontSize='large' className='header__icon'/>
             </IconButton>
-            <img className='header_logo'
+            <img className='header-_logo'
              src='https://logodownload.org/wp-content/uploads/2020/09/tinder-logo.png'
               alt=''
              />
