fa316a7f1a81ade7c64e7bb2f4de6f6bd70ee8a6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-03 03:09:37 +0500

    work complete

--- tinder/src/TinderCards.jsx
+++ tinder/src/TinderCards.jsx
@@ -58,7 +58,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfframe(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.imageurl})` }}
+              style={{ backgroundImage: `url(${person.image_url})` }}
               className="card"
             >
               <h1>{person.name}</h1>
